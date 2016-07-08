c03f824f6218bce087a7bb2e57958f789c24df78
Robert Richter <robert.c.richter@gmail.com>
2016-07-08 17:26:33 -0500

    Add rake, minitest, and travis build

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source "https://rubygems.org"
 
+gem "rake"
+gem "minitest"
 gem "twilio-ruby"
 gem "json"
 gem "sinatra"
---
+++ test/test.rb
@@ -0,0 +1,6 @@
+gem 'minitest'
+require 'minitest/autorun'
+
+class TestSamson < Minitest::Test
+  # Write something, please, I'm begging you
+end
